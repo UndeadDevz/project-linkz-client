88d96d9da2ec7da4188bd47f4c7ef10b0b9b9098
Danelarrate <danelarrate@gmail.com>
2024-07-24 15:56:45 -0300

    the most unique and orinal ui

---
+++ src/components/SortableItem.tsx
@@ -0,0 +1,66 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
+export function SortableItem(props: any) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className='w-full h-40 bg-white rounded-xl shadow-sm shadow-gray-400 flex overflow-hidden'
+    >
+      <div className='w-20 flex justify-center items-center ' {...listeners}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='icon icon-tabler icon-tabler-grip-vertical cursor-grab'
+          width='44'
+          height='44'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='#2c3e50'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M9 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+          <path d='M9 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+          <path d='M9 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+          <path d='M15 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+          <path d='M15 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+          <path d='M15 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0' />
+        </svg>
+      </div>
+      <div className='grow'>{props.id}</div>
+      <div className=' w-32 flex justify-center items-center'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='icon icon-tabler icon-tabler-trash'
+          width='44'
+          height='44'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='#2c3e50'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M4 7l16 0' />
+          <path d='M10 11l0 6' />
+          <path d='M14 11l0 6' />
+          <path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' />
+          <path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
+        </svg>
+      </div>
+    </div>
+  )
+}
--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,13 +1,71 @@
-import { SideBar } from "./sideBar/SideBar";
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { SideBar } from './sideBar/SideBar'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable'
+import { useState } from 'react'
+import { SortableItem } from '../components/SortableItem'
 
 export const Editor = () => {
+  function handleDragEnd(event: any) {
+    const { active, over } = event
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id)
+        const newIndex = items.indexOf(over.id)
+
+        return arrayMove(items, oldIndex, newIndex)
+      })
+    }
+  }
+  const [items, setItems] = useState([1, 2, 3])
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  )
+
   return (
-    <div className='w-full h-screen flex flex-row'>
-      <div className='w-1/6 p-2'>
+    <div className='w-full h-screen flex flex-row bg-gray-200'>
+      <div className='w-1/6 p-2 max-w-[400px] flex items-center justify-center py-4'>
         <SideBar />
       </div>
-      <div className='w-3/6'>Medio</div>
-      <div className='w-2/6'>Derecha</div>
+      <div className='grow flex justify-center pt-8'>
+        <div className=' p-2 flex flex-col gap-4 max-w-[700px] w-full '>
+          <div className='w-full h-16 text-xl bg-red-700  rounded-full text-center text-white justify-center flex items-center'>
+            + add link
+          </div>
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </DndContext>
+        </div>
+      </div>
+      <div className='w-2/6 xl:max-w-[570px] max-w-[400px]  items-center justify-center flex  border-l border-gray-300'>
+        <div className='xl:w-9/12 w-11/12 h-5/6 bg-blue-500 rounded-[3rem] shadow-lg shadow-gray-400 '></div>
+      </div>
     </div>
-  );
-};
+  )
+}
