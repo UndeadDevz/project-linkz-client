906628ab97025100ce3c8583d4ee761a0470aa8f
quantumvflux <marcosabaravalle@gmail.com>
2024-10-01 16:24:58 -0300

    para cristian

--- src/services/template.service.ts
+++ src/services/template.service.ts
@@ -12,3 +12,16 @@ export const getUserTemplates = async () => {
   const result = await response.json();
   return result;
 };
+
+export const getTemplate = async (id: string) => {
+  const url = `http://localhost:8080/template/${id}`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${Cookies.get("authToken")}`,
+    },
+  });
+
+  const result = await response.json();
+  return result;
+};
